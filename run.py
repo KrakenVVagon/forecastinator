2e55d527fb409adfd1fbb1401873095488a11b96
KrakenVVagon <a.younger.m@gmail.com>
2022-04-28 22:48:20 -0400

    update dash application
    now updated from button press
    button press to reverse input on graph
    styled upload

--- run.py
+++ run.py
@@ -10,20 +10,34 @@ import io
 
 app = dash.Dash()
 
-df = px.data.stocks()
-
 h1_style = {
         'textAlign':'center',
         'marginTop':40,
         'marginBottom':40
         }
 
+upload_style = {
+        'width':'100%',
+        'height':'60px',
+        'lineHeight':'60px',
+        'borderWidth':'1px',
+        'borderStyle':'dashed',
+        'borderRadius':'5px',
+        'textAlign':'center',
+        'margin':'10px'
+        }
+
 heading = html.H1(id='H1',children='Testing Dash Application',style=h1_style)
 dropdown = dcc.Dropdown(id='dropdown')
-upload= dcc.Upload(id='upload_data',children=html.Div(['Drag and Drop or ',html.A('Select Files')]),multiple=True)
+upload= dcc.Upload(
+        id='upload_data',
+        children=html.Div(['Drag and Drop or ',html.A('Select Files')]),
+        style=upload_style,multiple=True
+        )
 upload_graph = dcc.Graph(id='plot2')
+reverse_button = html.Button('Reverse it',id='reverse_button')
 
-app.layout = html.Div(id='parent',children=[heading,upload,dropdown,upload_graph])
+app.layout = html.Div(id='parent',children=[heading,upload,dropdown,upload_graph,reverse_button])
 
 def parse_upload(contents,filename):
     content_type, content_string = contents.split(',')
@@ -55,16 +69,24 @@ def update_dropdown(contents,filename):
 @app.callback(dd.Output(component_id='plot2',component_property='figure'),
         [dd.Input('upload_data','contents'),
          dd.Input('upload_data','filename'),
-         dd.Input('dropdown','value')]
+         dd.Input('dropdown','value'),
+         dd.Input('reverse_button','n_clicks')]
         )
-def upload_graph_update(contents,filename,dropdown_value):
+def upload_graph_update(contents,filename,dropdown_value,n):
+    ctx = dash.callback_context
     if contents:
         contents = contents[0]
         filename = filename[0]
         df = parse_upload(contents,filename)
 
         scatter = go.Scatter(x=df['day'],y=df[dropdown_value],mode='lines+markers')
-        fig = go.Figure(scatter)
+        data = [scatter]
+
+        if ctx.triggered[0]['prop_id'].split('.')[0] == 'reverse_button':
+            flipped = go.Scatter(x=df['day'],y=df[dropdown_value][::-1],mode='lines+markers')
+            data.append(flipped)
+
+        fig = go.Figure(data)
 
         fig.update_layout(
             title='Some Random Thing',
